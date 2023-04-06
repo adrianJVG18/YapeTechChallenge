b31cabcc002ebd5069ef5832281461d49c704045
adrianvalenzuela <adrian.jvg.18@gmail.com>
2023-04-06 00:05:42 -0300

    - Map is correctly supported for tablet

--- app/src/main/java/com/technical_challenge/yape/framework/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/technical_challenge/yape/framework/ui/fragment/MapFragment.kt
@@ -4,26 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.technical_challenge.yape.R
-import com.technical_challenge.yape.adapter.viewmodel.MapViewmodel
-import com.technical_challenge.yape.databinding.FragmentMapBinding
 import com.technical_challenge.yape.framework.model.RecipeUi
 import com.technical_challenge.yape.framework.model.deBundleRecipeUi
-import com.technical_challenge.yape.framework.utils.viewBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
 
-    private val binding: FragmentMapBinding by viewBinding(FragmentMapBinding::bind)
-    private val viewmodel: MapViewmodel by activityViewModels()
+    //private val binding: FragmentMapBinding by viewBinding(FragmentMapBinding::bind)
+    //private val viewmodel: MapViewmodel by activityViewModels()
 
-    private lateinit var mapFragment: SupportMapFragment
-    private var googleMap: GoogleMap? = null
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
 
     private lateinit var recipeUi: RecipeUi
 
@@ -31,33 +27,27 @@ class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         recipeUi = requireArguments().deBundleRecipeUi()
 
-        initializeMap()
-    }
-
-    override fun onMapReady(p0: GoogleMap) {
-        googleMap = p0
-
-        Log.d("MapFragment", "onMapReady successfully called")
-        updateLocationUi()
+        mapView = view.findViewById(R.id.map_view) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
 
     private fun updateLocationUi() {
         val coordinates = LatLng(recipeUi.location.latitude, recipeUi.location.longitude)
-        googleMap?.addMarker(
+        googleMap.addMarker(
             MarkerOptions()
                 .position(LatLng(recipeUi.location.latitude, recipeUi.location.longitude))
                 .title(recipeUi.name)
         ) ?: Log.d("MapFragment", "googleMap object has an issue")
         googleMap
-            ?.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15.0f))
-        mapFragment.onResume()
+            .moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15.0f))
+        mapView.onResume()
     }
 
-    private fun initializeMap() {
-        mapFragment = SupportMapFragment.newInstance()
-        childFragmentManager.beginTransaction()
-            .add(binding.mapView.id, mapFragment)
-            .commit()
-        mapFragment.getMapAsync(this)
+
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
+        updateLocationUi()
     }
 }
